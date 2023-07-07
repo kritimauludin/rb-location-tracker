ba4b12722588d16aee26a58ab10d312bd16792b6
Kriti M <macbookpro@macbooks-MacBook-Pro.local>
2023-07-07 14:50:40 +0700

    change login form design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+    {{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,5 +69,78 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
+
+    <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+                    <div class="d-flex justify-content-center py-4">
+                        <a href="index.html" class="logo d-flex align-items-center w-auto">
+                            <img src="{{ asset('assets') }}/img/logo.png" alt="">
+                        </a>
+                    </div><!-- End Logo -->
+
+                    <div class="card mb-3">
+
+                        <div class="card-body">
+
+                            <div class="pt-4 pb-2">
+                                <h5 class="card-title text-center pb-0 fs-4">{{ config('app.name', 'Radar Bogor Location Tracker') }}</h5>
+                                <p class="text-center small">Enter your email & password to login</p>
+                            </div>
+                            <form class="row g-3 needs-validation" method="POST" action="{{ route('login') }}">
+                                @csrf
+
+                                <div class="col-12">
+                                    <label for="email" class="text-md-end">{{ __('Email Address') }}</label>
+                                    <input id="email" type="email"
+                                        class="form-control @error('email') is-invalid @enderror" name="email"
+                                        value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+
+                                <div class="col-12">
+                                    <label for="password" class="text-md-end">{{ __('Password') }}</label>
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            {{ __('Forgot Your Password?') }}
+                                        </a>
+                                    @endif
+                                </div>
+                                <div class="col-12">
+                                    <button class="btn btn-primary w-100" type="submit">Login</button>
+                                </div>
+                                <div class="col-12">
+                                    <p class="small mb-0">Don't have account? <a
+                                            href="{{ route('register') }}">{{ __('Register') }}</a></p>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+
+                    <div class="credits">
+                        Designed by <a
+                            href="{{ config('app.url', 'http://localhost') }}">{{ config('app.name', 'Radar Bogor Location Tracker') }}</a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </section>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,21 +1,4 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-
-    <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-</head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
@@ -71,10 +54,73 @@
                 </div>
             </div>
         </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
     </div>
 </body>
+</html>
+
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Radar Bogor') }}</title>
+
+    <!-- Favicons -->
+    <link href="{{asset('assets')}}/img/favicon.png" rel="icon">
+    <link href="{{asset('assets')}}/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="{{asset('assets')}}/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{asset('assets')}}/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="{{asset('assets')}}/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="{{asset('assets')}}/vendor/quill/quill.snow.css" rel="stylesheet">
+    <link href="{{asset('assets')}}/vendor/quill/quill.bubble.css" rel="stylesheet">
+    <link href="{{asset('assets')}}/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="{{asset('assets')}}/vendor/simple-datatables/style.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="{{asset('assets')}}/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <main>
+        <div class="container">
+
+            @yield('content')
+
+        </div>
+    </main><!-- End #main -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="{{asset('assets')}}/vendor/apexcharts/apexcharts.min.js"></script>
+    <script src="{{asset('assets')}}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{asset('assets')}}/vendor/chart.js/chart.umd.js"></script>
+    <script src="{{asset('assets')}}/vendor/echarts/echarts.min.js"></script>
+    <script src="{{asset('assets')}}/vendor/quill/quill.min.js"></script>
+    <script src="{{asset('assets')}}/vendor/simple-datatables/simple-datatables.js"></script>
+    <script src="{{asset('assets')}}/vendor/tinymce/tinymce.min.js"></script>
+    <script src="{{asset('assets')}}/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="{{asset('assets')}}/js/main.js"></script>
+
+</body>
+
 </html>
