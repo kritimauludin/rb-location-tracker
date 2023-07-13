b73ce5dc5d44235f8ee1985d25f1d5d1bf631ea8
Kriti M <macbookpro@macbooks-MBP.lan>
2023-07-14 00:19:48 +0700

    add my-profile feature, change app layouts, change position component in home blade, add admin and courier count, add routes for my-profile web.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Customer;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,12 +25,24 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $admins = User::where('role_id', '2')->get();
+        $couriers = User::where('role_id', '3')->get();
         $customers = Customer::select('customer_name', 'join_date', 'expire_date', 'latitude', 'longitude', 'updated_at')->get();
+
+        //count
+        $totalAdmin = count($admins);
+        $totalCourier = count($couriers);
         $totalCustomer = count($customers);
-        // dd($customers);
+
         return view('home', [
             'customers' => $customers,
+            'totalAdmin' => $totalAdmin,
+            'totalCourier' => $totalCourier,
             'totalCustomers' => $totalCustomer
         ]);
+    }
+
+    public function myProfile(){
+        return view('my-profile');
     }
 }
